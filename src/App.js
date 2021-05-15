2a00f51108c529055b0152115a2efa290bf4acb5
Sid Hayoun Lee <sidhlee@gmail.com>
2021-05-15 13:00:35 -0400

    chore: setup & connect firebase database

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import { useEffect } from 'react';
+import firebase from './config/firebase';
 import ArticleForm from './components/ArticleForm';
 
+const userKey = 'demo';
+
 function App() {
+  useEffect(() => {
+    const articlesRef = firebase.database().ref(userKey);
+
+    const unsubscribe = articlesRef.on('value', (dataSnapshot) => {
+      const data = dataSnapshot.val();
+      console.log(data);
+    });
+    return unsubscribe;
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
