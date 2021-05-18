655432a04319e4562979e7d9bf1114e35d46e9ba
Sid Hayoun Lee <sidhlee@gmail.com>
2021-05-18 11:27:41 -0400

    improvement: center about page content + add footer

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,44 +2,50 @@ import React from 'react';
 import Logo from '../components/Logo';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Illust } from '../images/undraw_Calendar_re_ki49.svg';
+import Footer from '../components/Footer';
 
 function About() {
   return (
-    <div className={`About`}>
-      <div className="wiki">
-        <h2>Engram (neuropsychology)</h2>
-        <blockquote cite="https://en.wikipedia.org/wiki/Engram_(neuropsychology)">
-          <p>
-            An <strong>engram</strong> is a unit of cognitive information
-            imprinted in a physical substance, theorized to be the means by
-            which memories are stored as biophysical or biochemical changes in
-            the brain or other biological tissue, in response to external
-            stimuli.
-          </p>
-        </blockquote>
-        <figcaption>
-          -<cite>Wikipedia</cite>
-        </figcaption>
+    <>
+      <div className="about-wrapper">
+        <div className={`About`}>
+          <div className="wiki">
+            <h2>Engram (neuropsychology)</h2>
+            <blockquote cite="https://en.wikipedia.org/wiki/Engram_(neuropsychology)">
+              <p>
+                An <strong>engram</strong> is a unit of cognitive information
+                imprinted in a physical substance, theorized to be the means by
+                which memories are stored as biophysical or biochemical changes
+                in the brain or other biological tissue, in response to external
+                stimuli.
+              </p>
+            </blockquote>
+            <figcaption>
+              -<cite>Wikipedia</cite>
+            </figcaption>
+          </div>
+          <div className="what-is">
+            <Logo />
+            <p>
+              is a personal tracking system for online learning resources. Use
+              Engram to bookmark your favorite articles and tutorials, rate them
+              and create a side note. You'll have a deeper understanding of the
+              topic each time you revisit them. 💪 🧠 💡 <br />
+              No more losing track of countless read-later links!
+            </p>
+          </div>
+          <div className="back-to-main">
+            <Link className="button-bg" to="/">
+              Back to Engram
+            </Link>
+          </div>
+          <div className="illust-container">
+            <Illust className="illust" />
+          </div>
+        </div>
       </div>
-      <div className="what-is">
-        <Logo />
-        <p>
-          is a personal tracking system for online learning resources. Use
-          Engram to bookmark your favorite articles and tutorials, rate them and
-          create a side note. You'll have a deeper understanding of the topic
-          each time you revisit them. 💪 🧠 💡 <br />
-          No more losing track of countless read-later links!
-        </p>
-      </div>
-      <div className="back-to-main">
-        <Link className="button-bg" to="/">
-          Back to Engram
-        </Link>
-      </div>
-      <div className="illust-container">
-        <Illust className="illust" />
-      </div>
-    </div>
+      <Footer />
+    </>
   );
 }
 
